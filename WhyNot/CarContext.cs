f4f51ff2e1e231a0a1d0994fe95d231abb8459bb
Nicolas RAYNAUD <xnico2809@gmail.com>
2024-07-11 13:24:05 +0200

    Added Migrations

---
+++ WhyNot/CarContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyNot
+{
+    internal class CarContext : DbContext
+    {
+
+    }
+}
